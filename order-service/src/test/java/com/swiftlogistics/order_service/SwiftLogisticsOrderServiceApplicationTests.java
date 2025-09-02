e3b8d4cdec20f386528d2d1a156b9a5debe97cbe
Nishagi <jeewanthadeherath04@gmail.com>
2025-09-02 16:43:11 +0530

    initiated the Order Service

---
+++ order-service/src/test/java/com/swiftlogistics/order_service/SwiftLogisticsOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftlogistics.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftLogisticsOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
