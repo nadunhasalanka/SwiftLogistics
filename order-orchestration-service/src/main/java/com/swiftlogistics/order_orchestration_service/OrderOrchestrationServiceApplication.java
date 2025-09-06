9bfce57d69365b3a373e367ecdb7addebc38bbd4
Nishagi <jeewanthadeherath04@gmail.com>
2025-09-06 12:35:28 +0530

    adjusted the services to interact with discovery server without working on hard ports

--- order-orchestration-service/src/main/java/com/swiftlogistics/order_orchestration_service/OrderOrchestrationServiceApplication.java
+++ order-orchestration-service/src/main/java/com/swiftlogistics/order_orchestration_service/OrderOrchestrationServiceApplication.java
@@ -2,8 +2,10 @@ package com.swiftlogistics.order_orchestration_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderOrchestrationServiceApplication {
 
 	public static void main(String[] args) {
